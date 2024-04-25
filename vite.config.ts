ec3ee49edf3b10d26564e797121f2c914e57e3c7
YourDesTain <146116783+YourDesTain@users.noreply.github.com>
2024-04-25 18:20:50 +0300

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  plugins: [tsconfigPaths({ root: __dirname }), react()],
+  build: {
+    rollupOptions: {
+      external: ["/src/main.tsx"], 
+      output: {
+        manualChunks: {
+          antd: ["antd"],
+        },
+      },
+    },
+  },
+});
